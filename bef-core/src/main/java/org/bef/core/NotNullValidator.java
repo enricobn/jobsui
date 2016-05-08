0509a5b5f288c183f751a0235708216555be4f52
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-05-08 21:11:35 +0200

    first try of complexjob test resources

---
+++ bef-core/src/main/java/org/bef/core/NotNullValidator.java
@@ -0,0 +1,17 @@
+package org.bef.core;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by enrico on 4/30/16.
+ */
+public class NotNullValidator implements ParameterValidator<Object> {
+    @Override
+    public List<String> validate(Object value) {
+        if (value == null) {
+            return Collections.singletonList("Value is null.");
+        }
+        return Collections.emptyList();
+    }
+}
