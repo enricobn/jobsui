52e9f8c8b86fce06abd479f32f359385243cd8c3
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-05-08 22:39:28 +0200

    more on refactoring JobRunnerTest

--- bef-core/src/test/java/org/bef/core/JobRunnerTest.java
+++ bef-core/src/test/java/org/bef/core/JobRunnerTest.java
@@ -77,72 +77,7 @@ public class JobRunnerTest {
         FakeUiValue<String, ?> uiValueSurname = new FakeUiValue<>();
         when(ui.create(UIValue.class)).thenReturn(uiValueName, uiValueSurname);
 
-        final List<JobParameterDef<?>> parameterDefs = new ArrayList<>();
-
-        GroovyShell shell = new GroovyShell();
-
-        final JobParameterDefAbstract<String> name = new JobParameterDefGroovy<String>(
-                shell,
-                "name",
-                "Name",
-                String.class,
-                "import org.bef.core.ui.UIValue;\n" +
-                "import org.bef.core.ui.StringConverterString;\n" +
-                "\n" +
-                "def uiValue = ui.create(UIValue.class);\n" +
-                "uiValue.setConverter(new StringConverterString());\n" +
-                "uiValue.setDefaultValue(\"Enrico\");\n" +
-                "return uiValue;",
-                null,
-                null) {
-            @Override
-            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
-            }
-        };
-        parameterDefs.add(name);
-
-        final JobParameterDefAbstract<String> surname = new JobParameterDefGroovy<String>(
-                shell,
-                "surname",
-                "Surname",
-                String.class,
-                "def uiValue = ui.create(org.bef.core.ui.UIValue.class);\n" +
-                "uiValue.setConverter(new org.bef.core.ui.StringConverterString());\n" +
-                "return uiValue;",
-                null,
-                null) {
-        };
-        surname.addDependency(name);
-        parameterDefs.add(surname);
-
-        Job<String> job = new Job<String>() {
-            @Override
-            public String getName() {
-                return "Test";
-            }
-
-            @Override
-            public List<JobParameterDef<?>> getParameterDefs() {
-                return parameterDefs;
-            }
-
-            @Override
-            public JobFuture<String> run(final Map<String, Object> parameters) {
-                return new JobFuture<String>() {
-                    @Override
-                    public String get() {
-                        return parameters.get("name") + " " + parameters.get("surname");
-                    }
-                };
-            }
-
-            @Override
-            public List<String> validate(Map<String, Object> parameters) {
-                return Collections.emptyList();
-            }
-        };
-
-        final Future<JobFuture<String>> future = runJob(runner, job);
+        final Future<JobFuture<String>> future = runJob(runner, createGroovySimpleJob());
 
         window.waitUntilStarted();
 
@@ -163,102 +98,7 @@ public class JobRunnerTest {
 
         when(ui.create(UIChoice.class)).thenReturn(uiChoiceVersion, uiChoiceDb, uiChoiceUser);
 
-        final List<JobParameterDef<?>> parameterDefs = new ArrayList<>();
-
-        final JobParameterDefAbstract<String> version = new JobParameterDefAbstract<String>(
-                "version",
-                "Version",
-                String.class,
-                new NotEmptyStringValidator()) {
-            @Override
-            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
-                return ui.create(UIChoice.class);
-            }
-
-            @Override
-            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
-            }
-        };
-        parameterDefs.add(version);
-
-        final JobParameterDefAbstract<String> db = new JobParameterDefAbstract<String>(
-                "db",
-                "DB",
-                String.class,
-                new NotEmptyStringValidator()) {
-            @Override
-            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
-                return ui.create(UIChoice.class);
-            }
-
-            @Override
-            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
-                String version = (String) values.get("version");
-                if (version == null) {
-                    ((UIChoice)component).setItems(Collections.<String>emptyList());
-                } else if (version.equals("1.0")) {
-                    ((UIChoice)component).setItems(Arrays.asList("Dev-1.0", "Cons-1.0", "Dev"));
-                } else {
-                    ((UIChoice)component).setItems(Arrays.asList("Dev-2.0", "Cons-2.0", "Dev"));
-                }
-            }
-        };
-        parameterDefs.add(db);
-        db.addDependency(version);
-
-        final JobParameterDefAbstract<String> user = new JobParameterDefAbstract<String>(
-                "user",
-                "User",
-                String.class,
-                new NotEmptyStringValidator()) {
-            @Override
-            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
-                return ui.create(UIChoice.class);
-            }
-
-            @Override
-            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
-                String version = (String) values.get("version");
-                String db = (String) values.get("db");
-                if (version == null || db == null) {
-                    ((UIChoice)component).setItems(Collections.<String>emptyList());
-                } else {
-                    ((UIChoice)component).setItems(Arrays.asList(version + " " + db));
-                }
-            }
-        };
-        parameterDefs.add(user);
-        user.addDependency(db);
-        user.addDependency(version);
-
-        Job<String> job = new Job<String>() {
-            @Override
-            public String getName() {
-                return "Test";
-            }
-
-            @Override
-            public List<JobParameterDef<?>> getParameterDefs() {
-                return parameterDefs;
-            }
-
-            @Override
-            public JobFuture<String> run(final Map<String, Object> parameters) {
-                return new JobFuture<String>() {
-                    @Override
-                    public String get() {
-                        return (String) parameters.get("user");
-                    }
-                };
-            }
-
-            @Override
-            public List<String> validate(Map<String, Object> parameters) {
-                return Collections.emptyList();
-            }
-        };
-
-        final Future<JobFuture<String>> future = runJob(runner, job);
+        final Future<JobFuture<String>> future = runJob(runner, createComplexJob());
 
         window.waitUntilStarted();
 
@@ -274,7 +114,6 @@ public class JobRunnerTest {
         assertEquals("1.0 Dev-1.0", jobFuture.get());
         assertEquals(Arrays.asList("Dev-1.0", "Cons-1.0", "Dev"), uiChoiceDb.getItems());
         assertEquals(Arrays.asList("1.0 Dev-1.0"), uiChoiceUser.getItems());
-
     }
 
     private abstract class SimpleJobRunner {
@@ -366,6 +205,170 @@ public class JobRunnerTest {
                     @Override
                     public String get() {
                         return parameters.get("name") + " " + parameters.get("surname");
+                    }
+                };
+            }
+
+            @Override
+            public List<String> validate(Map<String, Object> parameters) {
+                return Collections.emptyList();
+            }
+        };
+
+    }
+
+    private Job<String> createGroovySimpleJob() {
+        GroovyShell shell = new GroovyShell();
+        final List<JobParameterDef<?>> parameterDefs = new ArrayList<>();
+
+        final JobParameterDefAbstract<String> name = new JobParameterDefGroovy<String>(
+                shell,
+                "name",
+                "Name",
+                String.class,
+                "import org.bef.core.ui.UIValue;\n" +
+                        "import org.bef.core.ui.StringConverterString;\n" +
+                        "\n" +
+                        "def uiValue = ui.create(UIValue.class);\n" +
+                        "uiValue.setConverter(new StringConverterString());\n" +
+                        "uiValue.setDefaultValue(\"Enrico\");\n" +
+                        "return uiValue;",
+                null,
+                null) {
+            @Override
+            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
+            }
+        };
+        parameterDefs.add(name);
+
+        final JobParameterDefAbstract<String> surname = new JobParameterDefGroovy<String>(
+                shell,
+                "surname",
+                "Surname",
+                String.class,
+                "def uiValue = ui.create(org.bef.core.ui.UIValue.class);\n" +
+                        "uiValue.setConverter(new org.bef.core.ui.StringConverterString());\n" +
+                        "return uiValue;",
+                null,
+                null) {
+        };
+        surname.addDependency(name);
+        parameterDefs.add(surname);
+
+        return new Job<String>() {
+            @Override
+            public String getName() {
+                return "Test";
+            }
+
+            @Override
+            public List<JobParameterDef<?>> getParameterDefs() {
+                return parameterDefs;
+            }
+
+            @Override
+            public JobFuture<String> run(final Map<String, Object> parameters) {
+                return new JobFuture<String>() {
+                    @Override
+                    public String get() {
+                        return parameters.get("name") + " " + parameters.get("surname");
+                    }
+                };
+            }
+
+            @Override
+            public List<String> validate(Map<String, Object> parameters) {
+                return Collections.emptyList();
+            }
+        };
+    }
+
+    private Job<String> createComplexJob() {
+        final List<JobParameterDef<?>> parameterDefs = new ArrayList<>();
+
+        final JobParameterDefAbstract<String> version = new JobParameterDefAbstract<String>(
+                "version",
+                "Version",
+                String.class,
+                new NotEmptyStringValidator()) {
+            @Override
+            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
+                return ui.create(UIChoice.class);
+            }
+
+            @Override
+            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
+            }
+        };
+        parameterDefs.add(version);
+
+        final JobParameterDefAbstract<String> db = new JobParameterDefAbstract<String>(
+                "db",
+                "DB",
+                String.class,
+                new NotEmptyStringValidator()) {
+            @Override
+            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
+                return ui.create(UIChoice.class);
+            }
+
+            @Override
+            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
+                String version = (String) values.get("version");
+                if (version == null) {
+                    ((UIChoice)component).setItems(Collections.<String>emptyList());
+                } else if (version.equals("1.0")) {
+                    ((UIChoice)component).setItems(Arrays.asList("Dev-1.0", "Cons-1.0", "Dev"));
+                } else {
+                    ((UIChoice)component).setItems(Arrays.asList("Dev-2.0", "Cons-2.0", "Dev"));
+                }
+            }
+        };
+        parameterDefs.add(db);
+        db.addDependency(version);
+
+        final JobParameterDefAbstract<String> user = new JobParameterDefAbstract<String>(
+                "user",
+                "User",
+                String.class,
+                new NotEmptyStringValidator()) {
+            @Override
+            public UIComponent createComponent(UI ui) throws UnsupportedComponentException {
+                return ui.create(UIChoice.class);
+            }
+
+            @Override
+            public void onDependenciesChange(UIComponent component, Map<String, Object> values) {
+                String version = (String) values.get("version");
+                String db = (String) values.get("db");
+                if (version == null || db == null) {
+                    ((UIChoice)component).setItems(Collections.<String>emptyList());
+                } else {
+                    ((UIChoice)component).setItems(Arrays.asList(version + " " + db));
+                }
+            }
+        };
+        parameterDefs.add(user);
+        user.addDependency(db);
+        user.addDependency(version);
+
+        return new Job<String>() {
+            @Override
+            public String getName() {
+                return "Test";
+            }
+
+            @Override
+            public List<JobParameterDef<?>> getParameterDefs() {
+                return parameterDefs;
+            }
+
+            @Override
+            public JobFuture<String> run(final Map<String, Object> parameters) {
+                return new JobFuture<String>() {
+                    @Override
+                    public String get() {
+                        return (String) parameters.get("user");
                     }
                 };
             }
