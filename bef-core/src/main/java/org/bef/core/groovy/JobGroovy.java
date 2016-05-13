f6f53b2bb07d50d1418e8975f3cde9ceee62ecd6
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-05-13 22:16:18 +0200

    cached groovy parsing

--- bef-core/src/main/java/org/bef/core/groovy/JobGroovy.java
+++ bef-core/src/main/java/org/bef/core/groovy/JobGroovy.java
@@ -1,6 +1,7 @@
 package org.bef.core.groovy;
 
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 import org.bef.core.JobAbstract;
 import org.bef.core.JobFuture;
 import org.bef.core.JobParameterDef;
@@ -13,20 +14,18 @@ import java.util.Map;
  * Created by enrico on 5/4/16.
  */
 public class JobGroovy<T> extends JobAbstract<T> {
-    private final GroovyShell shell;
     private final String key;
     private final String name;
     private final List<JobParameterDef<?>> parameterDefs;
-    private final String runScript;
-    private final String validateScript;
+    private final Script run;
+    private final Script validate;
 
     public JobGroovy(GroovyShell shell, String key, String name, List<JobParameterDef<?>> parameterDefs, String runScript, String validateScript) {
-        this.shell = shell;
         this.key = key;
         this.name = name;
         this.parameterDefs = parameterDefs;
-        this.runScript = runScript;
-        this.validateScript = validateScript;
+        this.run = shell.parse(runScript);
+        this.validate = validateScript == null ? null : shell.parse(validateScript);
     }
 
     @Override
@@ -44,19 +43,19 @@ public class JobGroovy<T> extends JobAbstract<T> {
         return new JobFuture<T>() {
             @Override
             public T get() {
-                shell.setProperty("values", parameters);
-                return (T) shell.evaluate(runScript);
+                run.setProperty("values", parameters);
+                return (T) run.run();
             }
         };
     }
 
     @Override
     public List<String> validate(Map<String, Object> parameters) {
-        if (validateScript == null) {
+        if (validate == null) {
             return Collections.emptyList();
         }
-        shell.setProperty("values", parameters);
-        return (List<String>) shell.evaluate(validateScript);
+        validate.setProperty("values", parameters);
+        return (List<String>) validate.run();
     }
 
     public String getKey() {
