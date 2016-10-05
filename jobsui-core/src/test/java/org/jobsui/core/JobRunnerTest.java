60cef63961bd61c7605bbd26e192df4bd72c13c7
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-10-05 13:35:49 +0200

    test: added external job

--- jobsui-core/src/test/java/org/jobsui/core/JobRunnerTest.java
+++ jobsui-core/src/test/java/org/jobsui/core/JobRunnerTest.java
@@ -285,6 +285,29 @@ public class JobRunnerTest {
         assertEquals(Collections.singletonList("Error"), window.getValidationMessages());
     }
 
+    @Test public void assert_that_groovy_external_concat_job_returns_the_correct_value_when_run_with_valid_parameters() throws Exception {
+        final FakeUiValue<String, ?> uiValueFirst = new FakeUiValue<>();
+        final FakeUiValue<String, ?> uiValueSecond = new FakeUiValue<>();
+        when(ui.create(UIValue.class)).thenReturn(uiValueFirst, uiValueSecond);
+
+        JobRunnerWrapper<String> jobRunnerWrapper = new JobRunnerWrapper<String>(runner, ui, window) {
+            @Override
+            protected void interact() {
+                uiValueFirst.setValue("first");
+                uiValueSecond.setValue("second");
+
+            }
+        };
+
+        JobParser parser = new JobParser();
+        Project project = parser.loadProject(new File("src/test/resources/external"));
+        final Job<String> job = project.getJob("concat");
+
+        final JobFuture<String> jobFuture = jobRunnerWrapper.start(job);
+
+        assertThat(jobFuture.get(), equalTo("firstsecond"));
+    }
+
     private Job<String> getMockedSimpleJob(FakeUiValue<String, ?> uiValueName, FakeUiValue<String, ?> uiValueSurname, FakeUIChoice uiChoiceInv) throws UnsupportedComponentException {
         final Job<String> job = mock(Job.class);
 
